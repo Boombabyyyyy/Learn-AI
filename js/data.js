3ce15810b0c45abd0acc00ed3e86a831dc712721
Izhan <babuizhan1234@gmail.com>
2022-07-04 15:30:11 +0530

    Added single single point of data ( pakka this time )

---
+++ js/data.js
@@ -0,0 +1,348 @@
+const homedata = [
+  {
+    url: 'course_details.html?q=0',
+    imageurl:
+      'https://www.freecodecamp.org/news/content/images/2021/08/chris-ried-ieic5Tq8YMk-unsplash.jpg',
+    topic: 'Programming',
+    name: 'Python for beginners',
+    diff: 'basic',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    rating: '4.0',
+    videos: '5 videos',
+    hours: '4 hours',
+    objectives: `To have a fundamental understanding of the Python programming language.
+    Have the skills and understanding of Python to confidently apply for Python programming jobs.
+    Acquire the pre-requisite Python skills to move into specific branches - Machine Learning, Data Science, etc..
+    Add the Python Object-Oriented Programming (OOP) skills to your résumé.
+    Understand how to create your own Python programs.
+    Learn Python from experienced professional software developers.
+    Understand both Python 2 and Python 3.`,
+  },
+  {
+    url: 'course_details.html?q=1',
+    imageurl:
+      'https://res.cloudinary.com/practicaldev/image/fetch/s--NOUFyZEg--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/5xtfavzdsdydctb0c83x.jpg',
+    topic: 'Programming',
+    name: `Javascript for beginners`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objectives: `Become an advanced, confident, and modern JavaScript developer from scratch
+    Build 6 beautiful real-world projects for your portfolio (not boring toy apps)
+    Become job-ready by understanding how JavaScript really works behind the scenes
+    How to think and work like a developer: problem-solving, researching, workflows
+    JavaScript fundamentals: variables, if/else, operators, boolean logic, functions, arrays, objects, loops, strings, etc.
+    Modern ES6+ from the beginning: arrow functions, destructuring, spread operator, optional chaining (ES2020), etc.
+    Modern OOP: Classes, constructors, prototypal inheritance, encapsulation, etc.`,
+  },
+  {
+    url: 'course_details.html?q=2',
+    imageurl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGfTsFC9lB-bJn93wDhkJmINznC1Kkov0hbA&usqp=CAU',
+    topic: 'Programming',
+    name: `Java for beginners `,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.18,000',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objectives: `Learn the core Java skills needed to apply for Java developer positions in just 4 hours.
+    Be able to sit for and pass the Oracle Java Certificate exam if you choose.
+    Be able to demonstrate your understanding of Java to future employers.
+    Learn industry "best practices" in Java software development from a professional Java developer who has worked in the language for 18 years.`,
+  },
+  {
+    url: 'course_details.html?q=3',
+    imageurl:
+      'https://www.coursesforfree.net/wp-content/uploads/2019/08/2497360_92b2.jpg',
+    topic: 'UI/UX',
+    name: `Figma UI/UX`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objectives: `How to begin working as a UX Designer using Figma.
+    How to use Figma for Essential UX Design & UI Design
+    How to make fully interactive prototypes.
+    How to work with a UX personas.
+    You will be able to add UX designer to your CV.
+    Build a UX project from beginning to end.
+    What the client expects of you as a UX designer.
+    How to create simple wireframes.`,
+  },
+  {
+    url: 'course_details.html?q=4',
+    imageurl:
+      'https://i.graphicmama.com/blog/wp-content/uploads/2019/04/11142949/online-graphic-design-courses-the-most-comprehensive-guide-11.jpg',
+    topic: 'Graphic Design',
+    name: `Graphic Design Masterclass`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objectives: `Have a deep understanding of typography, color theory, photos, layout, blocking and other design theory and skills
+    Understand and learn the essential tools and capabilities of Adobe Photoshop, Illustrator and InDesign
+    Create logos and branding packages for potential clients
+    Learn photo editing and manipulation in Adobe Photoshop
+    Master the pen tool, shape builder tool and other essential vector design tools in Adobe Illustrator.
+    How to create editorial spreads and magazine layouts using Adobe InDesign
+    `,
+  },
+  {
+    url: 'course_details.html?q=5',
+    imageurl:
+      'https://www.goacad.com/wp-content/uploads/2021/09/ppppsss-1-870x440.gif',
+    topic: 'Graphic Design',
+    name: `Adobe Photoshop`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objectives: `You will be able to start earning money from your Photoshop Skills.
+    You will be able to add Photoshop to your CV
+    You will have over 20 of your own projects to add to your portfolio.
+    You will create social media post images for Facebook, Instagram, Twitter, Pintrest & Youtube.
+    You will be able to mask anything… including the dreaded hair!
+    Clearcut images. Create amazing typography.
+    You will create popular visual styles.`,
+  },
+  {
+    url: 'course_details.html?q=6',
+    imageurl:
+      'https://www.bevyskills.com/wp-content/uploads/2021/03/canva-course.jpg',
+    topic: 'Graphic Design',
+    name: `Graphic Design using Canva`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objectives: `Learn Canva and the smart workflow approach that will continually save you time
+    Learn to create animated graphics, ebooks, videos, presentations, and much more
+    Learn to create custom templates that will allow you to stay on top of your content marketing
+    Learn graphic design fundamentals and how to easily apply them to your designs
+    Understand the differences between free and paid Canva and figure out which one is right for you
+    Stay up to date on all the latest Canva features`,
+  },
+  {
+    url: 'course_details.html?q=7',
+    imageurl:
+      'https://ezbrt4adg6k.exactdn.com/wp-content/uploads/2021/04/Adobe-Illustrator.jpg',
+    topic: 'Graphic Design',
+    name: `Adobe Illustrator`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objectives: `
+    Designing logos
+Creating vector illustrations
+Turning photographs into vector artwork
+Vectorizing and colorizing traced hand drawings
+Preparing graphics for web and print
+Designing infographics
+Working with type in creative ways
+Mastering advanced Illustrator tools and techniques
+    `,
+  },
+  {
+    url: 'course_details.html?q=8',
+    imageurl:
+      'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-pwa-course.png',
+    topic: 'Web dev',
+    name: `The complete guide to Angular development `,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objective: `Develop modern, complex, responsive and scalable web applications with Angular 14
+    Fully understand the architecture behind an Angular application and how to use it
+    Use the gained, deep understanding of the Angular fundamentals to quickly establish yourself as a frontend developer
+    Create single-page applications with one of the most modern JavaScript frameworks out there
+    `,
+  },
+  {
+    url: 'course_details.html?q=9',
+    imageurl:
+      'https://nareshit.com/wp-content/uploads/2019/01/ReactJS-online-training-nareshit.jpg',
+    topic: 'Web dev',
+    name: `The complete guide to React Development`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objective: `
+    Build powerful, fast, user-friendly and reactive web apps
+Provide amazing user experiences by leveraging the power of JavaScript with ease
+Apply for high-paid jobs or work as a freelancer in one the most-demanded sectors you can find in web dev right now
+Learn all about React Hooks and React Components
+    `,
+  },
+  {
+    url: 'course_details.html?q=10',
+    imageurl: 'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png',
+    topic: 'Web dev',
+    name: `The complete guide to Vue development`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objective: `
+    Build application with Vue 3
+Build an application with internationalization (i18n)
+Build an application with client side routing, by using vue router
+Implement global state management with Vuex
+    `,
+  },
+  {
+    url: 'course_details.html?q=11',
+    imageurl:
+      'https://res.cloudinary.com/practicaldev/image/fetch/s--sHTr-q9k--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://thepracticaldev.s3.amazonaws.com/i/3pj0ezd1q9vt0sdgx5r0.jpg',
+    topic: 'Mobile Development',
+    name: `Complete React Native course`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objective: `
+    Learn how to use ReactJS to build real native mobile apps for iOS and Android
+Develop cross-platform (iOS and Android) mobile apps without knowing Swift, ObjectiveC or Java/ Android
+Explore React Native basics and advanced features!
+Learn how to use key mobile app features like Google maps or the device camera
+    `,
+  },
+  {
+    url: 'course_details.html?q=12',
+    imageurl: 'https://mobignosis.com/wp-content/uploads/2019/10/Flutter.png',
+    topic: 'Mobile Development',
+    name: `Flutter & Dart- Complete Edition`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objective: `Learn Flutter and Dart from the ground up, step-by-step
+    Build engaging native mobile apps for both Android and iOS
+    Use features like Google Maps, the device camera, authentication and much more!
+    Learn how to upload images and how to send manual and automated push notifications`,
+  },
+  {
+    url: 'course_details.html?q=13',
+    imageurl:
+      'https://www.geeky-gadgets.com/wp-content/uploads/2017/07/Apple-Swift-3.jpg',
+    topic: 'Mobile Development',
+    name: `iOS & Swift - 2022`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objective: `You will learn Xcode, UIKit and SwiftUI, ARKit, CoreML and CoreData.
+    You will learn by doing, where every lesson is incorporated into a real-world app project.
+    After the course, you will be able to build any app you want.`,
+  },
+  {
+    url: 'course_details.html?q=14',
+    imageurl:
+      'https://images.shiksha.com/mediadata/images/articles/1580282235phpnjVoib.jpeg',
+    topic: 'Data Science',
+    name: `Complete Data Science course`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objective: `The course provides the entire toolbox you need to become a data scientist
+    Fill up your resume with in demand data science skills: Statistical analysis, Python programming with NumPy, pandas, matplotlib, and Seaborn, Advanced statistical analysis, Tableau, Machine Learning with stats models and scikit-learn, Deep learning with TensorFlow
+    Impress interviewers by showing an understanding of the data science field
+    Learn how to pre-process data
+    Understand the mathematics behind Machine Learning (an absolute must which other courses don’t teach!)
+    Start coding in Python and learn how to use it for statistical analysis`,
+  },
+  {
+    url: 'course_details.html?q=15',
+    imageurl:
+      'https://www.devopsschool.com/assets/assets1/images/certification/master-in-ai.jpg',
+    topic: 'AI',
+    name: `AI Masterclass`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objective: `How to Build an AI
+    How to Build a Hybrid Intelligent System
+    Fully-Connected Neural Networks
+    Convolutional Neural Networks
+    Recurrent Neural Networks
+    AutoEncoders`,
+  },
+  {
+    url: 'course_details.html?q=16',
+    imageurl:
+      'https://www.freecodecamp.org/news/content/images/2022/01/machine-learning-banner-2.png',
+    topic: 'ML',
+    name: `ML Masterclass`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objective: `The most effective method to dodge issues with Machine Learning, to effectively execute it without losing your brain!
+    To realise what issues Machine Learning can illuminate, and how the Machine Learning Process functions
+    Use Python for Machine Learning
+    Percentiles, moment and Quantiles
+    Learn to utilise Matplotlib for Python Plotting
+    Learn to utilise Seaborn for measurable plots`,
+  },
+  {
+    url: 'course_details.html?q=17',
+    imageurl:
+      'https://ssieducation.co.in/wp-content/uploads/2020/12/download-1.png',
+    topic: 'ML',
+    name: `Python for Machine Learning`,
+    diff: 'basic',
+    rating: '4.5',
+    orgprice: 'Rs.150',
+    price: 'Free',
+    videos: '5 videos',
+    hours: '4 hours',
+    objective: `Use Python for Data Science and Machine Learning
+    Use Spark for Big Data Analysis
+    Implement Machine Learning Algorithms
+    Learn to use NumPy for Numerical Data
+    Learn to use Pandas for Data Analysis
+    Learn to use Matplotlib for Python Plotting
+    Learn to use Seaborn for statistical plots
+    Use Plotly for interactive dynamic visualizations`,
+  },
+];
+
+export default homedata;
